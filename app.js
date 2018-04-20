5f201a895767292d128ee1f59835e000b059fd03
Leo1305Pineda <leo1305pineda@gmail.com>
2018-04-20 13:50:52 -0400

    Actualizando directorios

--- app.js
+++ app.js
@@ -2,39 +2,33 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-
 var app = express();
+
 app.set('secret', 'SECRET');
 var cons = require('consolidate');
 
 app.engine('html',cons.swig);
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
+app.use(logger('dev'));
 
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('No encontrado');
   err.status = 404;
   next(err);
 });
 
 app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({ error: true, mensaje: err.message || 'Fallo del servidor' });
+});
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+
+app.listen(PORT, function() {
+  console.log(`Servidor express corriendo en http://${HOST}:${PORT}`)
 });
 
 module.exports = app;
