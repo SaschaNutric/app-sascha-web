9ad3a9b207d1d47dd78a029b5a91825badb5899c
Leo1305Pineda <leo1305pineda@gmail.com>
2018-05-18 12:04:46 -0400

    reformando el projecto sascha-web

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var app = express();
 
@@ -10,8 +9,7 @@ var cons = require('consolidate');
 app.engine('html',cons.swig);
 app.set('view engine', 'html');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/sascha-web')));
 app.use(logger('dev'));
 
 app.use(function(req, res, next) {
